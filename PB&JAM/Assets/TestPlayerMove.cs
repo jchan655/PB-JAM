e9e657b8af77a069864378ec7d00ce007a4ae3a5
elee925 <74256170+elee925@users.noreply.github.com>
2021-01-29 18:57:02 -0500

    added test art and rigidbody

---
+++ PB&JAM/Assets/TestPlayerMove.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPlayerMove : MonoBehaviour
+{
+
+    public float moveSpeed = 10;
+    
+    private bool facingright = true;
+    private float moveDirection;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(moveDirection * moveSpeed, rb.velocity.y);
+    }
+}
